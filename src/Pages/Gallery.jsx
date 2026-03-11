7b6fe7a3d9a11ca693b8ee7a6dca4808ddb24836
krishjain4234 <24ug1bycs383@bmsit.in>
2026-03-11 20:12:21 +0530

    Revert "gallery updated"
    This reverts commit 59b02462a04d8ca0b32984190fe7cdc169eb40d5.

--- src/Pages/Gallery.jsx
+++ src/Pages/Gallery.jsx
@@ -3,13 +3,14 @@ import { X, ChevronLeft, ChevronRight } from 'lucide-react';
 import Footer from '../components/Footer/Footer';
 
 // Image Imports
-import img1 from '../assets/image.jpg';
+import img1 from '../assets/image1.jpg';
 import img2 from '../assets/image2.jpg';
 import img3 from '../assets/image3.jpg';
-import img4 from '../assets/image4.JPG';
-import img5 from '../assets/image5.JPG';
-import img6 from '../assets/image6.JPG';
+import img4 from '../assets/image4.jpg';
+import img5 from '../assets/image5.jpg';
+import img6 from '../assets/image6.jpg';
 import img7 from '../assets/image7.jpg';
+import img8 from '../assets/image8.jpg';
 import img9 from '../assets/image9.jpg';
 import img10 from '../assets/image10.jpg';
 import img11 from '../assets/image11.jpg';
@@ -53,7 +54,7 @@ const Gallery = () => {
   useEffect(() => {
     // Array of all images
     const allImages = [
-      img1, img2, img3, img4, img5, img6, img7, img9, img10,
+      img1, img2, img3, img4, img5, img6, img7, img8, img9, img10,
       img11, img12, img13, img14, img15, img16, img17, img18, img19, img20,
       img21, img22, img23, img24, img25, img26, img27, img28, img29, img30,
       img31, img32, img33, img34, img35, img36, img37, img38, img39, img40,
